199a07af47e731b4fe5dadb34f0463768315c756
Sarthak Panigrahi <sipunp70@gmail.com>
2025-02-23 21:24:52 +0530

    Fix Axios baseURL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -13,8 +13,6 @@ axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  } else {
-    console.warn("No token found in localStorage.");
   }
   return config;
 });
